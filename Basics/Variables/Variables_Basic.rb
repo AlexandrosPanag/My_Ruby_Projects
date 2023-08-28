066b5ade8e62ef795c3ffe5ec3ff3b093ef1f472
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-08-29 00:38:32 +0300

    Delete Variables_Basic.rb

--- Basics/Variables/Variables_Basic.rb
+++
@@ -1,9 +0,0 @@
-#By Alexandros Panagiotakopoulos - alexandrospanag.github.io
-#variable example
-
-
-example_variable_name = "John Doe"
-example_variable_age = "20"
-
-puts "Hello, my name is "+example_variable_name+" nice to meet you!" #example of putting a variable and text
-puts "My age is "+example_variable age #example of putting a variable
