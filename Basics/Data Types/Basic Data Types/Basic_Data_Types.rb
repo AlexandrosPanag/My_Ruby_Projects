00afe0546e9956ed509d7637ff8a7e2b0a3a1e18
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-08-29 00:55:03 +0300

    Add files via upload

---
+++ Basics/Data Types/Basic Data Types/Basic_Data_Types.rb
@@ -0,0 +1,9 @@
+#By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+#Basic Data Types
+
+
+var_one = 10
+var_two = 20.5
+var_string = "String_Example" #example of words (string)
+var_boolean = true #can be adjusted to true or false
+var_null = nil #null value ! it doesnt have anything!
