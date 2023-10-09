d3c1eff4a471aae41dc94e5d23355c3f4784550c
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-10-09 11:54:36 +0300

    Add files via upload

---
+++ Basics/Hashes/Basic Hashes Example.rb
@@ -0,0 +1,38 @@
+# By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+#source used: https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Glossary:Country_codes
+
+EU_countries = {
+  "Belgium"	=> "(BE)",
+  "Greece" =>	"(EL)",
+  "Lithuania"	=> "(LT)",
+  "Portugal" =>	"(PT)",
+  "Bulgaria" =>	"(BG)",
+  "Spain" =>	"(ES)",
+  "Luxembourg" =>	"(LU)",
+  "Romania" =>	"(RO)",
+  "Czechia" =>	"(CZ)",
+  "France" =>	"(FR)",
+  "Hungary" =>	"(HU)",
+  "Slovenia" =>	"(SI)",
+  "Denmark"	=> "(DK)",
+  "Croatia" =>	"(HR)",
+  "Malta" =>	"(MT)",
+  "Slovakia" =>	"(SK)",
+  "Germany"	=> "(DE)",
+  "Italy" =>	"(IT)",
+  "Netherlands" =>	"(NL)",
+  "Finland" =>	"(FI)",
+  "Estonia" =>	"(EE)",
+  "Cyprus" =>	"(CY)",
+  "Austria" =>	"(AT)",
+  "Sweden" =>	"(SE)",
+  "Ireland" =>	"(IE)",
+  "Latvia" =>	"(LV)",
+  "Poland" =>	"(PL)"
+}
+
+puts "COUNTRIES:\n"
+puts EU_countries
+
+puts "SPECIFIC COUNTRY KEY FIND:\n"
+puts EU_countries["Greece"]
