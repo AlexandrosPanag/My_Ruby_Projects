1617723384795a654467fb92db0820db9b2ec8c5
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-10-09 09:52:54 +0300

    Add files via upload

---
+++ Basics/Math Methods/Math Methods.rb
@@ -0,0 +1,12 @@
+# By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+mynum = -10.5431
+puts mynum.abs() #turn mynum into absolute value
+puts mynum.round() #round the number
+puts mynum.ceil() #ceil the number to the closest bigger one
+puts mynum.floor() #floor the number to the closest lowest one
+
+mynum2 = 25
+puts Math.sqrt(mynum2) #return the square root of a number
+puts Math.log(1) #returns the logarithm of 1
+#there are also many math functions for sine , cosine etc.
