a3e8c58abdf55068f692ba47a887f32b5c52a02a
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-10-11 09:08:46 +0300

    Add files via upload

---
+++ Basics/Case Statements/Switch Case Example.rb
@@ -0,0 +1,16 @@
+# By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+# Define a variable to switch on
+fruit = "apple"
+
+# Use a case statement to check the value of the variable
+case fruit
+when "apple"
+  puts "This is an apple"
+when "banana"
+  puts "This is a banana"
+when "orange"
+  puts "This is an orange"
+else
+  puts "I don't know what fruit this is"
+end
