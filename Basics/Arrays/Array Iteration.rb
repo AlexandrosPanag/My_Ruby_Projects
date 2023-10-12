4fd62c50a49bfefa64195affa8d0d835268e9ae3
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-10-12 17:06:08 +0300

    Add files via upload

---
+++ Basics/Arrays/Array Iteration.rb
@@ -0,0 +1,15 @@
+#Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+friends = ["Jim", "Smith", "Daniel", "Luis", "Jim"]
+
+# a basic array iteration
+puts "Basic array iteration"
+for i in friends
+  puts i
+end
+
+# a better array iteration
+puts "Better array iteration"
+friends.each do |i|
+  puts i
+end
