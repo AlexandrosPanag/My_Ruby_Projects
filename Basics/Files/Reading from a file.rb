7495b0b6556093685c6a1918af5e57ad1dc0196b
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-10-12 21:58:07 +0300

    Update Reading from a file.rb

--- Basics/Files/Reading from a file.rb
+++ Basics/Files/Reading from a file.rb
@@ -1,8 +1,9 @@
 #Alexandros Panagiotakopoulos - alexandrospanag.github.io
 
 
-file = File.open("C:/Users/alexa/Desktop/students.txt", "r") do |file|
-  file.each_line do |line|
+file = File.open("C:/Users/<username>/Desktop/students.txt", "r") do |file| #use your own path this is an example
+  file.each_line do |line| 
     puts line
+    #optional code to read from a single line puts file.readline()
   end
 end
