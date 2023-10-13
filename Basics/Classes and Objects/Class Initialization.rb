765ce69d5fb99d95f80c03bd8859dc2a8ccfa0af
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-10-13 09:21:12 +0300

    Update Class Initialization.rb

--- Basics/Classes and Objects/Class Initialization.rb
+++ Basics/Classes and Objects/Class Initialization.rb
@@ -4,7 +4,7 @@
 class Student
   attr_accessor :name, :age, :grade
   def initialize(name, age, pages) #initialize is a special method that is called when you create a new object
-    puts ("New student created! Hello!" + name)
+    puts ("New student created! Hello! " + name)
     @name = name
     @age = age
     @grade = grade
