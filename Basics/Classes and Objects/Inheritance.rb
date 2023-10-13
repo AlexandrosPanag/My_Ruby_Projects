4e493aa083f2badeffbf649739e707b7f10f4c2b
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-10-13 09:52:19 +0300

    Update Inheritance.rb

--- Basics/Classes and Objects/Inheritance.rb
+++ Basics/Classes and Objects/Inheritance.rb
@@ -8,7 +8,7 @@ class Junior
 end
 
 #class senior
-class Senior < Junior
+class Senior < Junior #Inherit the method from junior
   def senior
     puts "This programmer also knows ruby, python, java, javascript"
   end
@@ -17,4 +17,5 @@ end
 jrprogrammer = Junior.new
 jrprogrammer.beginner
 srprogrammer = Senior.new
+srprogrammer.beginner #note how senior inherited the method from junior without creating a def begginer method
 srprogrammer.senior
